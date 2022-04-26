0679b1e9468312dca47fce934d868ae8cbb0f7bc
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-26 23:34:59 +0900

    [ADD] 순수 JDBC 실습 (#31)

--- src/main/java/hello/HelloSpring/SpringConfig.java
+++ src/main/java/hello/HelloSpring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.HelloSpring;
 
+import hello.HelloSpring.repository.JdbcMemberRepository;
 import hello.HelloSpring.repository.MemberRepository;
 import hello.HelloSpring.repository.MemoryMemberRepository;
 import hello.HelloSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        return new JdbcMemberRepository(dataSource);
     }
 }
